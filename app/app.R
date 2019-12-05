0d17f4788b81aa4e7ceb4927d40cecbc7b803b89
艾熙尧 <aixiyao_aiden@aidandeMac.local>
2019-12-05 03:49:46 -0500

    Ai's part

--- app/app.R
+++ app/app.R
@@ -1,6 +1,43 @@
 library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(broom)
+library(ggstance)
+library(shinythemes)
+
+
+
+college_type <- c("Public", "private nonprofit","private for-profit")
+student_type <- c("Independent", "Dependent")
+
+
 
 ui <- fluidPage(
+  titlePanel("College Score Card"),
+  tabsetPanel(
+    tabPanel("Basic Information",
+             sidebarLayout(
+               sidebarPanel(width = 4,
+                            ),
+               mainPanel(width = 8)
+             )),
+    tabPanel("Jack's"),
+    tabPanel("Family Income",
+             sidebarLayout(
+               sidebarPanel(width = 4,
+                            numericInput("income", "Family Income", value = 0),
+                            radioButtons("college_type", "Which type of college do you prefer to check?",
+                                         choices = college_type),
+                            radioButtons("Student_type", "Which type of student do you prefer to check?",
+                                         choices = student_type)),
+               mainPanel(width = 8,
+                         plotOutput("boxplot"),
+                         tableOutput("test"))
+             )),
+    tabPanel("Sam's")
+  )
+  
+  
   
 )
 
